f980631b5c2ae6de5c2a33f3214502c6dce17e88
GlenTiki <GlenTiki@users.noreply.github.com>
2020-02-12 17:26:15 +0000

    deploy: aff66e7f007e1302d8a090b4dc3b1feec4c6f925

---
+++ precache-manifest.bd6bd9ca1db96032509b3ef30f12c689.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13ab3cb473f704cef449fda46e89ee07",
+    "url": "/WaterfordTech/WaterfordTech.github.io/index.html"
+  },
+  {
+    "revision": "8bf8001fd3fef71c4ca2",
+    "url": "/WaterfordTech/WaterfordTech.github.io/static/css/main.dfb21432.chunk.css"
+  },
+  {
+    "revision": "877832c114c1ed48a1fd",
+    "url": "/WaterfordTech/WaterfordTech.github.io/static/js/2.45c2d83c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/WaterfordTech/WaterfordTech.github.io/static/js/2.45c2d83c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bf8001fd3fef71c4ca2",
+    "url": "/WaterfordTech/WaterfordTech.github.io/static/js/main.f7002afc.chunk.js"
+  },
+  {
+    "revision": "0188265bcda21017a685",
+    "url": "/WaterfordTech/WaterfordTech.github.io/static/js/runtime-main.b8c7cd9d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/WaterfordTech/WaterfordTech.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
