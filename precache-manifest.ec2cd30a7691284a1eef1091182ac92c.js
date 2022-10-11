51c2eb4656cd93f22083fa765eb5b6c96d6fab88
GlenTiki <GlenTiki@users.noreply.github.com>
2022-10-11 15:16:07 +0000

    deploy: 0d580bd90d332b7dbdd705161b89b3a976b858b7

---
+++ precache-manifest.ec2cd30a7691284a1eef1091182ac92c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee700c4504d49b3d9c890e62e5ddcc6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14e017ee9b0bfa9202e4",
+    "url": "/static/js/2.690214d0.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.690214d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e754c6ab1d4a3b758089",
+    "url": "/static/js/main.a6e99df7.chunk.js"
+  },
+  {
+    "revision": "5926b3a359d67a67c8f0",
+    "url": "/static/js/runtime-main.bbaa1b7d.js"
+  },
+  {
+    "revision": "0946fd0a1ab85d68edfaef9f41738203",
+    "url": "/static/media/landing-splash-vertical.0946fd0a.jpeg"
+  },
+  {
+    "revision": "01c4e6ad7d47c9dc93dfef30bed9eb30",
+    "url": "/static/media/landing-splash.01c4e6ad.jpeg"
+  }
+]);
